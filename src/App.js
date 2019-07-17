889bb40fc5a4c6c5938a1cb754ecd0d15eece89d
Christopher Pupo <chrispupo@Christophers-iMac.local>
2019-07-17 19:48:59 -0400

    set up the api to get the data back

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Posts from "./components/Posts.js"
+import axios from 'axios'
 import "./App.css";
+//import src from "*.svg";
 
 function App() {
+  const [data, setData] = useState([])
+
+  const fetch = () => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=v6BWrD5polvAAE2wbGy1WHGC35mIllgAGeqCfJHK')
+      .then(res => {  
+        setData(res.data)
+      })
+      .catch(error => { console.log('t-minus 10 seconds, error!')}
+      )
+    }
+
+    useEffect(fetch, [])
+
+    console.log(data)
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Posts nasaVid={data.url} title={data.title} />
     </div>
   );
 }
